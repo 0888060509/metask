58f3215cac33f96ff197027335939c84c21e3f4b
Cường Huỳnh <cuong.huynh@eggstech.io>
2025-09-22 12:14:11 +0000

    remove the x button in detail task (_for element <X>_)

--- src/components/kanban/task-detail-dialog.tsx
+++ src/components/kanban/task-detail-dialog.tsx
@@ -254,7 +254,10 @@ export function TaskDetailDialog({
                             </DropdownMenu>
                           </>
                         )}
-                        <SheetClose />
+                        <SheetClose className="ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+                          <X className="h-4 w-4" />
+                          <span className="sr-only">Close</span>
+                        </SheetClose>
                     </div>
                 </div>
             </SheetHeader>
